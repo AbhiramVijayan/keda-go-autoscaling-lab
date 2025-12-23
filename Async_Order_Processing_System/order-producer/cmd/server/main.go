fc698ce24201a24ba99d9049462a0c80a3918f96
Abhiram Vijayan <abhiramvijayan1@gmail.com>
2025-12-23 13:49:11 +0000

    Implement Order Producer API with HTTP server and add module configuration

--- Async_Order_Processing_System/order-producer/cmd/server/main.go
+++ Async_Order_Processing_System/order-producer/cmd/server/main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Order Producer API starting...")
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("Order Producer API is running "))
+	})
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: mux,
+	}
+	log.Println("🚀 Order Producer API listening on port 8080")
+
+	log.Fatal(server.ListenAndServe())
+
 }
