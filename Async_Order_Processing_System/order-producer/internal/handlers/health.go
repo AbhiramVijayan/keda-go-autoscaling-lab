edb06f08384b34829700d98112e373e6614b2cea
Abhiram Vijayan <abhiramvijayan1@gmail.com>
2025-12-26 14:27:09 +0000

    Update code and improve documentation

--- Async_Order_Processing_System/order-producer/internal/handlers/health.go
+++ Async_Order_Processing_System/order-producer/internal/handlers/health.go
@@ -1,13 +1,23 @@
 package handlers
 
-import "net/http"
+import (
+	"log/slog"
+	"net/http"
+	"os"
+)
+
+var logger = slog.New(slog.NewJSONHandler(os.Stdout, nil))
 
 func HealthCheckHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodGet {
 		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		logger.Error("Invalid method for HealthCheckHandler", "method", r.Method)
+
 		return
 	}
 	w.Header().Set("Content-Type", "application/json")
+	logger.Info("Health check accessed", "request_uri", r.RequestURI, "method", r.Method)
+
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte(`{"status":"healthy"}`))
 }
--- Async_Order_Processing_System/order-producer/internal/handlers/order.go
+++ Async_Order_Processing_System/order-producer/internal/handlers/order.go
@@ -21,16 +21,20 @@ type OrderResponse struct {
 func CreateOrderHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
 		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		logger.Error("Invalid method for CreateOrderHandler", "request_uri", r.RequestURI, "method", r.Method)
+		// logger.Error("Invalid method for CreateOrderHandler", "method", r.Method)
 		return
 	}
 	var req OrderRequest
 	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
 		http.Error(w, "invalid JSON payload", http.StatusBadRequest)
+		logger.Error("invalid JSON payload", "request_uri", r.RequestURI, "error", err)
 		return
 	}
 
 	if req.OrderID == "" || req.UserID == "" || req.Item == "" || req.Quantity <= 0 {
 		http.Error(w, "invalid request fields", http.StatusBadRequest)
+		logger.Error("invalid request fields", "request_uri", r.RequestURI, "request", req)
 		return
 	}
 	_ = time.Now().UTC()
@@ -41,4 +45,6 @@ func CreateOrderHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusAccepted)
 	json.NewEncoder(w).Encode(resp)
+	logger.Info("Order created", "request_uri", r.RequestURI, "order_id", req.OrderID, "user_id", req.UserID)
+
 }
